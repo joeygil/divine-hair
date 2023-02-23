c07af036d8bdb9258bd71f6d87cf7ab46e653528
joeygil <105308164+joeygil@users.noreply.github.com>
2023-02-23 10:37:12 +0000

    footer additions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
+import { UilFacebook, UilInstagramAlt } from "@iconscout/react-unicons";
+
 const Footer = () => {
   return (
     <>
       <div className="relative">
-        <div className="absolute   z-0 h-[250px] w-screen bg-[#725C71]">
-          <h1>HELLO</h1>
+        <div className="absolute z-0 h-[100px] w-screen bg-[#725C71]">
+          <div className="footer-items flex justify-around items-center mt-5">
+            <UilFacebook size="50" color="white" />
+            <UilInstagramAlt size="50" color="white" />
+            <p className="footer-items text-5xl text-white">01423 864755</p>
+            <p className="footer-items text-3xl text-white">info@divinehair.com</p>
+          </div>
         </div>
       </div>
     </>
